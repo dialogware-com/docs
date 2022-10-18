99571164340e71e0738818f7d51c5063b475f566
tom-sapletta-com <tom@sapletta.com>
2022-10-18 20:27:17 +0200

    update docs

--- push.sh
+++ push.sh
@@ -1,6 +1,22 @@
+#/bin/bash -e
+#GITHUB_TOKEN
+[ -z $GITHUB_TOKEN ] && GITHUB_TOKEN=$(cat .github_token)
 flatedit
 cp -f README.md docs/index.md
 yarn docs:build
 cp -f -r vitepress/.vitepress/dist/* .
-git commit -m "update docs"
-git push
+
+MESSAGE=$1
+MESSAGE="update docs"
+[ -z $MESSAGE ] && read -p "Enter message for commit: " MESSAGE
+git add . # To add the changes in local repository
+git commit -m"${MESSAGE}" # To commit the changes and prepare it for remote repository
+if [ -n "$(git status - porcelain)" ];
+then
+ echo "IT IS CLEAN"
+else
+ git status # To check the status of working tree
+ echo "Pushing data to remote server!!!"
+ git push -u origin master # To push the changes in remote repository
+fi
+
--- vitepress/.vitepress/config.js
+++ vitepress/.vitepress/config.js
@@ -5,15 +5,13 @@ export default defineConfig({
         lang: 'en-US',
         title: 'DIALOGWARE',
         description: 'Text to Software solution to build a web application in minutes',
-        themeConfig: {
-            logo: '/dialogware-2lines.png',
-            siteTitle: false,
-            nav: [
-                {text: 'Guide', link: '/guide'},
-                {text: 'Configs', link: '/configs'},
-                {text: 'Changelog', link: 'https://github.com/...'}
-            ]
-        },
+        logo: '/dialogware-2lines.png',
+        siteTitle: false,
+        nav: [
+            {text: 'Guide', link: '/guide'},
+            {text: 'Configs', link: '/configs'},
+            {text: 'Changelog', link: 'https://github.com/...'}
+        ],
         markdown: {
             theme: 'material-palenight',
             lineNumbers: true
