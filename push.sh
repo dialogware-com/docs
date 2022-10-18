c524726c5cb037798789c968d0217e526602e3c4
tom-sapletta-com <tom@sapletta.com>
2022-10-18 20:32:08 +0200

    update docs

--- push.sh
+++ push.sh
@@ -10,14 +10,20 @@ cp -f -r vitepress/.vitepress/dist/* .
 MESSAGE=$1
 MESSAGE="update docs"
 [ -z $MESSAGE ] && read -p "Enter message for commit: " MESSAGE
-git add . # To add the changes in local repository
-git commit -m"${MESSAGE}" # To commit the changes and prepare it for remote repository
+git status # To check the status of working tree
+
 if [ -n "$(git status - porcelain)" ];
 then
  echo "IT IS CLEAN"
 else
+ git add . # To add the changes in local repository
+ git commit -m"${MESSAGE}" # To commit the changes and prepare it for remote repository
  git status # To check the status of working tree
- echo "Pushing data to remote server!!!"
- git push -u origin master # To push the changes in remote repository
 fi
+
+#git add . # To add the changes in local repository
+git commit -a -m"${MESSAGE}" # To commit the changes and prepare it for remote repository
+
+echo "Pushing data to remote server!!!"
+git push -u origin master # To push the changes in remote repository
 
