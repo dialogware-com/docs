6b71ea0503140a95d0b9a77e583252c5efada8a1
tom-sapletta-com <tom@sapletta.com>
2022-10-18 17:15:27 +0200

    update webpage

---
+++ vitepress/.vitepress/config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vitepress'
+
+export default defineConfig({
+    themeConfig: {
+        lang: 'en-US',
+        title: 'DIALOGWARE',
+        description: 'Text to Software solution to build a web application in minutes',
+        themeConfig: {
+            logo: '/dialogware-2lines.png',
+            siteTitle: false,
+            nav: [
+                {text: 'Guide', link: '/guide'},
+                {text: 'Configs', link: '/configs'},
+                {text: 'Changelog', link: 'https://github.com/...'}
+            ]
+        },
+        markdown: {
+            theme: 'material-palenight',
+            lineNumbers: true
+        }
+    }
+})
